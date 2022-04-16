0dd4467a7b8fa22c674aee8981c559b759879615
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-04-16 16:39:24 -0600

    Additional ALUV cases

--- microarquitectura/Execute/VECTOR_ALU/ALUV.sv
+++ microarquitectura/Execute/VECTOR_ALU/ALUV.sv
@@ -1,6 +1,6 @@
 module ALUV #(parameter DATA_WIDTH = 8, 
 				parameter LANES = 6,
-				parameter SELECTOR_SIZE = 2)
+				parameter SELECTOR_SIZE = 3)
 			( 
 				 input [SELECTOR_SIZE-1:0] selector,
 				 input [LANES-1:0][DATA_WIDTH-1:0] operand1,
@@ -53,17 +53,26 @@ module ALUV #(parameter DATA_WIDTH = 8,
 			
 	always_comb begin  
       case (selector)  
-			2'b00 : begin
+			3'b000 : begin
 				out = {outSumador[5], outSumador[4], outSumador[3], outSumador[2], outSumador[1], outSumador[0]};				
 			end
-			2'b01 : begin
+			3'b001 : begin
 				out = {outRestador[5], outRestador[4], outRestador[3], outRestador[2], outRestador[1], outRestador[0]};				
 			end
-			2'b10 : begin
+			3'b010 : begin
 				out = {outMultiplicador[5], outMultiplicador[4], outMultiplicador[3], outMultiplicador[2], outMultiplicador[1], outMultiplicador[0]};				
 			end
-			2'b11 : begin
+			3'b011 : begin
 				out = {outDivisor[5], outDivisor[4], outDivisor[3], outDivisor[2], outDivisor[1], outDivisor[0]};				
+			end
+			3'b100 : begin
+				out = {operand1[5], operand1[4], operand1[3], operand1[2], operand1[1], operand1[0]};				
+			end
+			3'b101 : begin
+				out = {operand2[5], operand2[4], operand2[3], operand2[2], operand2[1], operand2[0]};				
+			end
+			default begin
+				out = {operand1[5], operand1[4], operand1[3], operand1[2], operand1[1], operand1[0]};
 			end
       endcase 		
    end
