b99986635d2f2587abc6c8a1f7cdf4070b67299d
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-04-22 12:46:56 -0600

    Edit conditional unit

--- microarquitectura/Control/condunit.sv
+++ microarquitectura/Control/condunit.sv
@@ -6,10 +6,9 @@ module condunit #(parameter OPCODEWIDTH = 4)
 		
 	always_comb
 		case(opcodeE)
-			4'b1011: takeBranchE = Z; // EQ
+			4'b1100: takeBranchE = Z; // EQ
 			4'b1101: takeBranchE = N!=V; // LT
-			4'b1100: takeBranchE = 1'b1; // ??
-			4'b1111: takeBranchE = 1'b1; // ??
+			4'b1110: takeBranchE = 1'b1; // JUMP
 			default: takeBranchE = 1'b0; // 0
 		endcase
 	
